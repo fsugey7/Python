bafad080c2eaa2b761809929925fb2ea87ea7469
sugey.sugey <sugey,flores@soymas.cl>
2023-07-06 00:04:15 -0400

    z

---
+++ 22-z.py
@@ -0,0 +1,10 @@
+for y in range(10):
+
+    for z in range(10):
+     if y == 0 or y == 10 - 1 or y + z == 10 - 1:
+          print("*", end =" " )
+     else:
+            print(" ", end = " ")
+    print()
+
+# 
